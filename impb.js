2b791f0c7158270ab95d2e28941513bb909079cf
steadyoak <62462010+steadyoak@users.noreply.github.com>
2022-04-04 16:32:10 +0900

    Update impb.js

--- impb.js
+++ impb.js
@@ -3,7 +3,8 @@
 var imgbu = window.location.href;
 alert(imgbu);
 // if(imgbu == "https://www.imagebam.com/") {
-// const hinp = document.querySelector(".dz-hidden-input");
+const hinp = document.querySelector(".dz-hidden-input");
+hinp.click();
 // hinp.addEventListener("change", function(){
 // document.querySelector("[value='nsfw']").click();
 // document.querySelector(".btn").click();
